008e1b8a6f1eb8834ebc0f7005967b7226455489
klemerson01 <kleminho.rodrigues0102@gmail.com>
2022-05-26 16:54:00 -0300

    calculadora js

---
+++ script.js
@@ -0,0 +1,25 @@
+
+// var resp = document.getElementById("resp");
+
+
+
+function calc(x, y, usu) {
+  switch (usu) {
+    case 1:
+      return x + y;
+      break;
+    case 2:
+      return x - y;
+      break;
+    case 3:
+      return x * y;
+      break;
+    case 4:
+      return x / y;
+      break;
+  }
+
+}
+
+
+
